d8e4a4cf6983eae283309363523bc555a3c02c4f
cdtest <corydavis1989@gmail.com>
2022-01-29 15:53:29 -0600

    ready for heroku

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { notes } = require("./db/db");
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
+  });
+
+app.listen(3001, () => {
+  console.log(`API server now on port 3001!`);
+});
