6f47caf83090f7a627897c67c45d4ff890cf8bc9
cdtest <corydavis1989@gmail.com>
2022-01-29 16:27:21 -0600

    added notes route

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -22,6 +23,10 @@ app.post("/api/notes", (req, res) => {
 
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
   });
 
   app.listen(PORT, () => {
